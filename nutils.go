a7db73ad6caaecc815456abd8a36334bf711616e
nubela <nubela@gmail.com>
2015-05-20 16:50:35 +0800

    added bytestostring() and makenonchunkencodingrequest()

--- nutils.go
+++ nutils.go
@@ -3,13 +3,34 @@ import (
 	"io"
 	"bytes"
 	"unsafe"
+	"net/http"
+	"io/ioutil"
 )
 
 // this function converts a Reader type into string
 // note: it does it unsafely, altho efficiently.
+// note2: this will consumer the Reader's buffer
 func ReaderToString(reader io.Reader) string {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(reader)
 	b := buf.Bytes()
-	return *(*string)(unsafe.Pointer(&b))
+	return BytesToString(b)
+}
+
+func BytesToString(bytes []byte) string {
+	return *(*string)(unsafe.Pointer(&bytes))
+}
+
+// this takes a http.Request obj, removes `chunked encoding` header, and adds a Content-Length header by calculating
+// the Body size.
+func MakeNonChunkEncodingRequest(r *http.Request) *http.Request {
+	delete(r.Header, "Transfer-Encoding")
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	i := len(buf.Bytes())
+	r.ContentLength = int64(i)
+	r.Body = ioutil.NopCloser(buf)
+
+	return r
 }
